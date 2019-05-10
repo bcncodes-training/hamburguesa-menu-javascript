6424dee3bc083475e67dcf2db29dc0301b282f79
rglepe <37378026+rglepe@users.noreply.github.com>
2019-05-10 12:26:12 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,20 @@ let navigation = document.querySelector('.navigation');
 function drawCloseIcon(){
     if(navicon.classList.contains('open')){
         navicon.classList.replace('open','close');
+        navigation.classList.replace('open','close');
     }
     else{
         navicon.classList.replace('close','open');
+        navigation.classList.replace('close','open');
+    
     }
 }
 
+function resetClassOpen(){
+    navicon.classList.replace('close','open');
+    navigation.classList.replace('close','open');
+}
+
+window.onresize= resetClassOpen;
 
 navicon.onclick = drawCloseIcon;
